833cf5e0f05a50bc35636f5f2838217b9fefa56e
rokon007 <rezaulkrm85@gmail.com>
2022-03-01 04:18:12 +0600

    add

---
+++ database/seeders/TiteleAndTagSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TiteleAndTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('titetle_and_tags')->insert([
+            'title' => 'Vimbiso',
+            'tags' => 'Vimbiso',
+            'deteles' => 'Vimbiso - A free, independent & Transparent customer assurance platform. Giving consumers a powerful voice to enable business to listen, respond and improve',
+            
+        ]);
+    }
+}
