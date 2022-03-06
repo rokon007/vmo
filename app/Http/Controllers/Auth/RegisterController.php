9729dcd2467ad792d63272cbcca1f0e54f14f560
rokon007 <rezaulkrm85@gmail.com>
2022-03-06 16:35:36 +0600

    edit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -43,14 +43,14 @@ class RegisterController extends Controller
 
     /**
      * Get a validator for an incoming registration request.
-     *
+     *      
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'company_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],           
             
@@ -67,11 +67,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'company_name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'companycontact' => $data['companycontact'],
-            'address' => $data['address'],
+            'contact' => $data['companycontact'],
+            'is_superuser' => $data['address'],
         ]);
     }
 }
