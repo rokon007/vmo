8c69dfba9ba9fc9f13309798a59a5c93ea6507b7
rokon007 <rezaulkrm85@gmail.com>
2022-03-06 14:45:12 +0600

    users_column

---
+++ database/migrations/2022_03_06_083404_add_multiple_column_to_users.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMultipleColumnToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable();
+            $table->string('last_login')->nullable();
+            $table->string('is_superuser')->nullable();
+            $table->string('groups')->nullable();
+            $table->string('user_permissions')->nullable();
+            $table->string('username')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('is_staff')->nullable();
+            $table->string('is_active')->nullable();
+            $table->timestamp('date_joined')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('block')->nullable();
+            $table->string('contact')->nullable();
+            $table->string('level')->nullable();
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
