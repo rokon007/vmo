bbc24d3f91be8a236cd518dfa1e8ab5ea6bbba14
rokon007 <rezaulkrm85@gmail.com>
2022-03-06 16:07:55 +0600

    edit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+           'username' => 'Super Admin',
+            'email' => 'rokon07@hotmail.com',
+            'password' => Hash::make('rokon007'),
+            'is_superuser' => '1',
+            'company_name' => 'Admin',
+            'contact' => '+8801717524792',
+            
+        ]);
+    }
+}
