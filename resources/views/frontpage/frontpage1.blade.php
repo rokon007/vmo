cab11f6bdb1587c15cee339d65f339f26a19315e
rokon007 <rezaulkrm85@gmail.com>
2022-03-15 20:18:59 +0600

    edit

--- resources/views/frontpage/frontpage1.blade.php
+++ resources/views/frontpage/frontpage1.blade.php
@@ -1012,7 +1012,7 @@ ul {
 
 
      <!-- SCRIPTS -->
-     <script src={{(asset"js/jquery.js")}}></script>
+     <script src={{asset("js/jquery.js")}}></script>
      <script src={{asset("js/bootstrap.min.js")}}></script>
      <script src={{asset("js/owl.carousel.min.js")}}></script>
      <script src={{asset("js/smoothscroll.js")}}></script>
