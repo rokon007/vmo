dfcd6f6f72f1ded90991d471d518ac4f94469c23
rokon007 <rezaulkrm85@gmail.com>
2022-04-08 03:34:16 +0600

    edite

--- resources/views/frontpage/companybycategory1.blade.php
+++ resources/views/frontpage/companybycategory1.blade.php
@@ -188,7 +188,7 @@
 						 @endif
 						style="background-color:#e9e9ed;padding:0px !important">
 					  
-                          
+                    
                       
                           
        <a href="/profile/{{$Data->company}}"> 
@@ -286,6 +286,51 @@
             ''
         </script>
 <!-- Modal -->
-         @include('frontpage.give_review');
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+     <div class="modal-header metallic-red-bg">
+                <div class="d-flex justify-content-center ">
+                    <h3 class="modal-title text-white text-center " id="exampleModalLongTitle">Give a review</h3>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true" class="text-white">&times;</span>
+                </button>
+            </div>
+      <div class="modal-body">
+       <div class="form-group d-flex flex-row">
+					 <div class="col-lg-12 col-sm-12 p-0 pr-3">
+					 <div class="form-group">
+					 <a href="/auth/redirectgoogle"><button class="btn  form-control"><img src="https://img.icons8.com/color/48/000000/google-logo.png"/> Continue with Google</button></a>
+					 </div>
+						</div>
+						</div>
+		 <div class="form-group d-flex flex-row">				
+						
+						 <div class="col-lg-12 col-sm-12 p-0 pr-3">
+					 <div class="form-group">
+					  <a href="/auth/redirectfacebook"><button class="btn  form-control"> <img src="https://img.icons8.com/cute-clipart/48/000000/facebook.png"/> Continue with FaceBook</button></a>
+					 </div>
+						</div>
+						</div>
+						
+			 <div class="form-group d-flex flex-row">			
+                         <div class="col-lg-12 col-sm-12 p-0 pr-3">
+                     <div class="form-group">
+                      <a href="/review"><button class="btn  form-control"><img <img src="https://vimbisotest.herokuapp.com/static/images/favicon.ico"/> Continue with Vimbiso</button></a>
+                     </div>
+                        </div>
+						</div>
+						
+					 
+                    
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
--- resources/views/frontpage/profile.blade.php
+++ resources/views/frontpage/profile.blade.php
@@ -72,6 +72,8 @@
 									 55%
 								 @elseif(round($ratings/($reviewscount-1))==4)
 									 75%
+								  @elseif(round($ratings/($reviewscount-1))>=4)
+                                     90%								  
 								  @elseif(round($ratings/($reviewscount-1))==5)
 									   100%
 								  @elseif(round($ratings/($reviewscount-1))>=5)
@@ -92,6 +94,8 @@
 									 55%
 								 @elseif(round($ratings/($reviewscount-1))==4)
 									 75%
+								  @elseif(round($ratings/($reviewscount-1))>=4)
+                                     90%	 
 								  @elseif(round($ratings/($reviewscount-1))==5)
 									  100%
 								  @elseif(round($ratings/($reviewscount-1))>=5)
@@ -118,6 +122,8 @@
 									 60%
 								 @elseif(round($ratings/($reviewscount-1))==4)
 									 80%
+								  @elseif(round($ratings/($reviewscount-1))>=4)
+                                     90%	 
 								  @elseif(round($ratings/($reviewscount-1))==5)
 									  100%
 								  @elseif(round($ratings/($reviewscount-1))>=5)
@@ -138,6 +144,8 @@
 									 60%
 								 @elseif(round($ratings/($reviewscount-1))==4)
 									 80%
+								  @elseif(round($ratings/($reviewscount-1))>=4)
+                                     90%	 
 								  @elseif(round($ratings/($reviewscount-1))==5)
 									  100%
 								  @elseif(round($ratings/($reviewscount-1))>=5)
@@ -287,5 +295,50 @@
 
  @endif
  <!-- Modal -->
-         @include('frontpage.give_review');
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+     <div class="modal-header metallic-red-bg">
+                <div class="d-flex justify-content-center ">
+                    <h3 class="modal-title text-white text-center " id="exampleModalLongTitle">Give a review</h3>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true" class="text-white">&times;</span>
+                </button>
+            </div>
+      <div class="modal-body">
+       <div class="form-group d-flex flex-row">
+					 <div class="col-lg-12 col-sm-12 p-0 pr-3">
+					 <div class="form-group">
+					 <a href="/auth/redirectgoogle"><button class="btn  form-control"><img src="https://img.icons8.com/color/48/000000/google-logo.png"/> Continue with Google</button></a>
+					 </div>
+						</div>
+						</div>
+		 <div class="form-group d-flex flex-row">				
+						
+						 <div class="col-lg-12 col-sm-12 p-0 pr-3">
+					 <div class="form-group">
+					  <a href="/auth/redirectfacebook"><button class="btn  form-control"> <img src="https://img.icons8.com/cute-clipart/48/000000/facebook.png"/> Continue with FaceBook</button></a>
+					 </div>
+						</div>
+						</div>
+						
+			 <div class="form-group d-flex flex-row">			
+                         <div class="col-lg-12 col-sm-12 p-0 pr-3">
+                     <div class="form-group">
+                      <a href="/review"><button class="btn  form-control"><img <img src="https://vimbisotest.herokuapp.com/static/images/favicon.ico"/> Continue with Vimbiso</button></a>
+                     </div>
+                        </div>
+						</div>
+						
+					 
+                    
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
